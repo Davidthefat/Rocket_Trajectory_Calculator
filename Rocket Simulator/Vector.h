548c3372e495dd0d98bf946f363bc498ec933882
Davidthefat <davidthefat@gmail.com>
2015-02-01 17:46:01 -0700

    Added main.cpp and Vector.h

---
+++ Rocket Simulator/Vector.h
@@ -0,0 +1,42 @@
+#pragma once
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <array>
+
+enum coord{X, Y, Z};
+
+template <class T, std::size_t N >
+class Vector;
+
+template <class T, std::size_t N >
+Vector<T, N> operator*(Vector<T, N> left, const Vector<T, N>& right);
+template <class T, std::size_t N >
+Vector<T, N> operator*(Vector<T, N> left, T right);
+template <class T, std::size_t N >
+Vector<T, N> operator+(Vector<T, N> left, const Vector<T, N>& right);
+template <class T, std::size_t N >
+Vector<T, N> operator+(Vector<T, N> left, T right);
+
+//Start declaration of class.
+template <class T, std::size_t N >
+class Vector
+{
+private:
+	std::array<T,N> *Values;
+public:
+	Vector();
+	Vector<T, N> &operator=(const Vector<T, N>& right);
+	T &operator[](const int& right);
+	Vector<T, N> &operator+=(const Vector<T, N>& right);
+	Vector<T, N> &operator*=(const Vector<T, N>& right);
+	Vector<T, N> &operator+=(T right);
+	Vector<T, N> &operator*=(T right);
+
+	friend Vector<T, N> operator*<T, N>(Vector<T, N> left, const Vector<T, N>& right);
+	friend Vector<T, N> operator*<T, N>(Vector<T, N> left, T right);
+	friend Vector<T, N> operator+<T, N>(Vector<T, N> left, const Vector<T, N>& right);
+	friend Vector<T, N> operator+<T, N>(Vector<T, N> left, T right);
+
+	const T getValue(size_t) const;
+	void setValue(size_t, const T);
+};
---
+++ Rocket Simulator/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Vector.h"
+#include "Vector.cpp"
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+	Vector<double, 3> test;
+	Vector<double, 3> test2;
+	test.setValue(0, 1.0);
+	test.setValue(1, 1.0);
+	test.setValue(2, 1.0);
+	test2.setValue(0, 7.0);
+	test2.setValue(1, 3.0);
+	test2.setValue(2, 5.0);
+	cout << test[X] << "\t" << test[Y] << "\t" << test[Z] << endl;
+	test = test2 * 2.0;
+	cout << test[X] << "\t" << test[Y] << "\t" << test[Z] << endl;
+	
+	return 0;
+}
