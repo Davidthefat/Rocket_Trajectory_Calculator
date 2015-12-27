a6d681831dbbc908937e80243ebc5f18838c18be
David Yoon <davidthefat@davids-mbp>
2015-12-26 21:30:39 -0800

    Initial Commit

---
+++ TrajectorySim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TrajectorySim
+//
+//  Created by David Yoon on 12/26/15.
+//  Copyright © 2015 David Yoon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
